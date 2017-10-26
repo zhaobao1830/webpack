d96feaf2cbf8d2854079a2c66cb67a37cb284424
zhaobao1830 <x5219438>
2017-10-27 00:25:15 +0800

    第十一节练习

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,4 @@
 // 路径一定要用相对路径，以防其他操作系统上不能用
 import css from './css/index.css';
+import less from './css/black.less';
 document.getElementById("title").innerHTML = "hell zys";
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const uglify=require('uglifyjs-webpack-plugin');
 const htmlPlugin=require('html-webpack-plugin');
 const extractTextPlugin = require("extract-text-webpack-plugin");
 var website ={
-  publicPath:"http://192.168.0.103:1717/"
+  publicPath:"http://192.168.0.102:1717/"
 }
 module.exports={
   entry:{
@@ -29,9 +29,26 @@ module.exports={
         use:[{
           loader:'url-loader',
           options:{
-            limit:500000
+            limit:5000,
+            outputPath:'images/'
           }
         }]
+      },
+      {
+        test: /\.(htm|html)$/i,
+        use:[ 'html-withimg-loader']
+      },
+      {
+        test: /\.less$/,
+        use: extractTextPlugin.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "less-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
       }
     ]
   },
@@ -45,11 +62,12 @@ module.exports={
       hash:true,
       template:'./src/index.html'
     }),
-    new extractTextPlugin("/css/index.css")
+    // p配置打包后css的位置和名字
+    new extractTextPlugin("css/index.css")
   ],
   devServer:{
     contentBase:path.resolve(__dirname,'dist'),
-    host:'192.168.0.103',
+    host:'192.168.0.102',
     compress:true,
     port:1717
   }
